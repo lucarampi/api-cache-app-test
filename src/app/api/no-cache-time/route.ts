3f27c08560471316b6a33eee77eaae4a88fca41a
Luca Alfaro Rampinelli <lucar@weg.net>
2023-06-30 09:44:04 -0300

    test 2

--- src/app/api/no-cache-time/route.ts
+++ src/app/api/no-cache-time/route.ts
@@ -1,10 +1,8 @@
 import { NextResponse } from 'next/server'
 
-export const fetchCache = 'force-no-store'
-// 'auto' | 'default-cache' | 'only-cache'
-// 'force-cache' | 'force-no-store' | 'default-no-store' | 'only-no-store'
-
 export async function GET() {
     const newDate = new Date().toUTCString().toString();
     return NextResponse.json({ date: newDate })
 }
+
+export const fetchCache = 'only-no-store'
