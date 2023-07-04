08f0d88a091325fdff02f706a6b7fe071e407161
Luca Alfaro Rampinelli <lucar@weg.net>
2023-07-04 11:47:26 -0300

    test10

--- src/app/api/no-cache-time/route.ts
+++ src/app/api/no-cache-time/route.ts
@@ -1,11 +1,7 @@
 import { NextConfig, PageConfig } from 'next';
 import { NextResponse, NextRequest } from 'next/server'
-import { revalidatePath } from 'next/cache';
+
 export async function GET(req:NextRequest) {
-    const path =req.nextUrl.searchParams.get("path")|| "/"
-    console.log("🚀 ~ file: route.ts:6 ~ GET ~ path:", path)
     const newDate = new Date().toUTCString().toString();
     return NextResponse.json({ date: newDate })
 }
-
-
